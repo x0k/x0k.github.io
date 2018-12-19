1107b9bc01e79f5e2dc2a7d0e1eabad254f00a68
Roman <x0kill@ya.ru>
2018-12-19 17:47:15 +0300

    Initial version

---
+++ precache-manifest.593ecef3dbd6efadb971091f216cae4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f252ab39043eaf17799",
+    "url": "/project-schedule/static/js/runtime~main.6f252ab3.js"
+  },
+  {
+    "revision": "87f28ed756bc16d49df3",
+    "url": "/project-schedule/static/js/main.87f28ed7.chunk.js"
+  },
+  {
+    "revision": "119b6a8996908e283e2b",
+    "url": "/project-schedule/static/js/1.119b6a89.chunk.js"
+  },
+  {
+    "revision": "87f28ed756bc16d49df3",
+    "url": "/project-schedule/static/css/main.a63db26b.chunk.css"
+  },
+  {
+    "revision": "791face24a17cf9100aa134930344505",
+    "url": "/project-schedule/index.html"
+  }
+];
