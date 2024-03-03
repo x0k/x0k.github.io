6f1519cb3330e836b2720b2463227b6c727d73cd
Krasilnikov Roman <x0kill@ya.ru>
2024-03-04 00:14:43 +0300

    WIP

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,19 @@
 import { Lang, Page } from './shared';
 
 export enum Label {
+  Name = "person.name",
   ResumePage = "page.resume",
   ProjectsPage = "page.projects",
 }
 
 const strings: Record<Lang, Record<Label, string>> = {
   [Lang.RU]: {
+    [Label.Name]: "Красильников Р.Б.",
     [Label.ResumePage]: "Резюме",
     [Label.ProjectsPage]: "Проекты",
   },
   [Lang.EN]: {
+    [Label.Name]: "R.B. Krasilnikov",
     [Label.ResumePage]: "VC",
     [Label.ProjectsPage]: "Projects",
   },
