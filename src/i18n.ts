7c951d7ef2267d2ce3e34bed15f0bf487191c97c
Krasilnikov Roman <x0kill@ya.ru>
2024-03-04 19:33:44 +0300

    badges

--- src/i18n.ts
+++ src/i18n.ts
@@ -18,10 +18,9 @@ export enum Label {
   Name = "person.name",
   ResumePage = "page.resume",
   ProjectsPage = "page.projects",
+  Lang = "project.lang",
   Stack = "project.stack",
   Infra = "project.infra",
-  Link = "project.link",
-  Platforms = "project.platforms",
 }
 
 const strings: Record<Lang, Record<Label, string>> = {
@@ -29,19 +28,17 @@ const strings: Record<Lang, Record<Label, string>> = {
     [Label.Name]: "Красильников Р.Б.",
     [Label.ResumePage]: "Резюме",
     [Label.ProjectsPage]: "Проекты",
-    [Label.Stack]: "Стек",
+    [Label.Lang]: "Языки",
+    [Label.Stack]: "Технологии",
     [Label.Infra]: "Инфраструктура",
-    [Label.Link]: "Ссылка",
-    [Label.Platforms]: "Платформа",
   },
   [Lang.EN]: {
     [Label.Name]: "R.B. Krasilnikov",
     [Label.ResumePage]: "VC",
     [Label.ProjectsPage]: "Projects",
-    [Label.Stack]: "Stack",
+    [Label.Lang]: "Languages",
+    [Label.Stack]: "Technologies",
     [Label.Infra]: "Infrastructure",
-    [Label.Link]: "Link",
-    [Label.Platforms]: "Platforms",
   },
 };
 
