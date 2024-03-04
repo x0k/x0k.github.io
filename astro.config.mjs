04a80fe15b2de061b3970cb3206b9572c619d42f
Krasilnikov Roman <x0kill@ya.ru>
2024-03-04 19:38:40 +0300

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://x0k.github.io",
   integrations: [tailwind(), icon()],
   i18n: {
     defaultLocale: "ru",
