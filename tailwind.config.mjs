b4cf595e58ce2b37b9be44d653c50ba895f97dd5
Krasilnikov Roman <x0kill@ya.ru>
2024-08-08 22:50:05 +0300

    Markup improvements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
