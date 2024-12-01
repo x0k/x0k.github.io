c70e1f312e2d64956732f6468bf83075af007886
Krasilnikov Roman <x0kill@ya.ru>
2024-12-02 02:18:26 +0300

    Add stars count for github projects

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -6,6 +6,15 @@ export interface GetTopRatedReposOptions {
   topN?: number;
 }
 
+function parseLink(link: string) {
+  const url = new URL(link);
+  if (url.host !== "github.com") {
+    return null;
+  }
+  const [owner, repo] = url.pathname.split("/").slice(1, 3);
+  return { owner, repo };
+}
+
 export async function getTopStarredActualRepos(
   client: Octokit,
   { username, topN = 5, perPage = 50 }: GetTopRatedReposOptions
@@ -19,4 +28,27 @@ export async function getTopStarredActualRepos(
   return repos
     .filter((r) => !r.archived && (r.stargazers_count ?? 0) > 0)
     .slice(0, topN);
+}
+
+export async function getReposStarts(
+  client: Octokit,
+  repos: string[]
+): Promise<Record<string, number>> {
+  const result: Record<string, number> = {};
+  await Promise.all(
+    repos.map(async (repo) => {
+      const repoInfo = parseLink(repo);
+      if (repoInfo === null) {
+        return;
+      }
+      const {
+        data: { stargazers_count },
+      } = await client.repos.get(repoInfo);
+      if (stargazers_count === undefined) {
+        return;
+      }
+      result[repo] = stargazers_count;
+    })
+  );
+  return result;
 }
